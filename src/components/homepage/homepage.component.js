91173f86e8e3bb16bf3f2bbce4ac12435ae468dc
Jorge <jdesilvestro@gmail.com>
2020-09-10 17:20:37 +0100

    - Breaking the homepage into smaller components and adding some style.

---
+++ src/components/homepage/homepage.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './homepage.styles.scss'
+import Directory from "../directory/directory.component";
+
+const Homepage = () => {
+    return <div className="homepage">
+        <Directory/>
+    </div>
+}
+
+export default Homepage;
