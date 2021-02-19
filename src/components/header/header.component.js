0ae26b2c88c3e299e99cd81e2dffc04675ad4633
ortsevlised <jdesilvestro@gmail.com>
2021-02-19 07:53:42 +0000

    Fixing missing images

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {ReactComponent as Logo} from '../../assets/jojo_logo.svg';
+import {ReactComponent as Logo} from '../../assets/cypress.svg';
 import './header.styles.scss';
 import {auth} from "../../firebase/firebase.utils";
 import {connect} from "react-redux";
@@ -13,14 +13,11 @@ import {selectCartHidden} from "../../redux/cart/cart.selector";
 const Header = ({currentUser, hidden}) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
-            <Logo className='logo'/>
+            <Logo/>
         </Link>
         <div className='options'>
             <Link className='option' to='/shop'>
                 SHOP
-            </Link>
-            <Link className='option' to='/shop'>
-                CONTACT
             </Link>
             {currentUser ?
                 <div className='option'
--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -3,33 +3,33 @@ const INITIAL_STATE = {
         {
             'id': 1,
             'item': 'hats',
-            'imgUrl': 'https://scontent.fdub4-1.fna.fbcdn.net/v/t1.0-9/118950433_327360588706292_2675987918412598464_o.jpg?_nc_cat=100&_nc_sid=9e2e56&_nc_ohc=JOzrpkL8uN4AX8SF_K-&_nc_ht=scontent.fdub4-1.fna&oh=ae43c0636f4ae6da770c7fed3e8c7986&oe=5F7E7D69',
+            'imgUrl': 'https://images.unsplash.com/photo-1552399230-e073362b3bf4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
             'url': 'shop/hats'
         },
         {
             'id': 2,
             'item': 'jackets',
-            'imgUrl': 'https://scontent.fdub4-1.fna.fbcdn.net/v/t1.0-9/118296035_319350409507310_7348987858056941915_o.jpg?_nc_cat=104&_nc_sid=9267fe&_nc_ohc=zqXaPRlIttsAX_EJwr5&_nc_ht=scontent.fdub4-1.fna&oh=2373c18b3cb48103fdff6d36e0cf2eaf&oe=5F7E909B',
+            'imgUrl': 'https://images.unsplash.com/photo-1601924994987-69e26d50dc26?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
             'url': 'shop/jackets'
         },
         {
             'id': 3,
             'item': 'sneakers',
-            'imgUrl': 'https://scontent.fdub4-1.fna.fbcdn.net/v/t1.0-9/75051720_121262712649415_2623811833412190208_o.jpg?_nc_cat=111&_nc_sid=9267fe&_nc_ohc=6OP6NNpkg2kAX9khnKZ&_nc_ht=scontent.fdub4-1.fna&oh=8696c83006f011acc4c7f68c96216dc6&oe=5F7F0C98',
+            'imgUrl': 'https://images.unsplash.com/photo-1462927114214-6956d2fddd4e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1349&q=80',
             'url': 'shop/sneakers'
         },
         {
             'id': 4,
             'size': 'large',
             'item': 'womens',
-            'imgUrl': 'https://scontent.fdub4-1.fna.fbcdn.net/v/t1.0-9/118762115_322605955848422_653675378369032305_o.jpg?_nc_cat=103&_nc_sid=a26aad&_nc_ohc=4UX6q145AqQAX-_PY_3&_nc_ht=scontent.fdub4-1.fna&oh=20b1328c79b19dcc24e524adfa61e8e5&oe=5F803F2B',
+            'imgUrl': 'https://images.unsplash.com/photo-1500643752441-4dc90cda350a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=657&q=80',
             'url': 'shop/womens'
         },
         {
             'id': 5,
             'item': 'mens',
             'size': 'large',
-            'imgUrl': 'https://scontent.fdub4-1.fna.fbcdn.net/v/t1.0-9/97948531_244518343657184_5611733178654916608_o.jpg?_nc_cat=107&_nc_sid=9267fe&_nc_ohc=gRuRHTfGupYAX-Epgt7&_nc_ht=scontent.fdub4-1.fna&oh=fb1f953fefd6f7ec9a0dc5088542b920&oe=5F7F81A4',
+            'imgUrl': 'https://images.unsplash.com/photo-1589270216117-7972b3082c7d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
             'url': 'shop/mens'
         }
     ]
